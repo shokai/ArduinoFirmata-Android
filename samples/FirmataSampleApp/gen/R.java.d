3f224f38f06cb2cfbe85c09fa3482d2a1b6f86af
Sho Hashimoto <hashimoto@shokai.org>
2012-12-09 02:44:13 +0900

    blink arduino led

--- samples/FirmataSampleApp/gen/R.java.d
+++ samples/FirmataSampleApp/gen/R.java.d
@@ -1,4 +1,5 @@
 /Users/sho/src/android/ArduinoFirmata/samples/FirmataSampleApp/gen/org/shokai/firmata/sample/R.java \
  : /Users/sho/src/android/ArduinoFirmata/samples/FirmataSampleApp/res/layout/main.xml \
 /Users/sho/src/android/ArduinoFirmata/samples/FirmataSampleApp/res/values/strings.xml \
+/Users/sho/src/android/ArduinoFirmata/samples/FirmataSampleApp/res/xml/device_filter.xml \
 /Users/sho/src/android/ArduinoFirmata/samples/FirmataSampleApp/bin/AndroidManifest.xml \
--- samples/FirmataSampleApp/gen/org/shokai/firmata/sample/R.java
+++ samples/FirmataSampleApp/gen/org/shokai/firmata/sample/R.java
@@ -14,6 +14,9 @@ public final class R {
         public static final int main=0x7f020000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+    }
+    public static final class xml {
+        public static final int device_filter=0x7f030000;
     }
 }
