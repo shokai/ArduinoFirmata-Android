ff7ac0ea284e7eff5d0dafc183b7fd8f28b9ee69
Sho Hashimoto <hashimoto@shokai.org>
2012-12-15 01:35:20 +0900

    release v0.0.4

--- src/org/shokai/firmata/ArduinoFirmata.java
+++ src/org/shokai/firmata/ArduinoFirmata.java
@@ -11,7 +11,7 @@ import android.content.*;
 import android.util.Log;
 
 public class ArduinoFirmata{
-    public final static String VERSION = "0.0.3";
+    public final static String VERSION = "0.0.4";
     public final static String TAG = "ArduinoFirmata";
 
     public static final byte INPUT  = 0;
