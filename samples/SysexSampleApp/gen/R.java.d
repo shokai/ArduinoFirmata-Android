2c7f63d3cbc792c4e7b9a5aff24ba86e0ac1f981
Sho Hashimoto <hashimoto@shokai.org>
2013-01-21 13:49:53 +0900

    create project for sysex command sample  #14

---
+++ samples/SysexSampleApp/gen/R.java.d
@@ -0,0 +1,4 @@
+/Users/sho/src/android/ArduinoFirmata/samples/SysexSampleApp/gen/org/shokai/firmata/sysexsample/R.java \
+ : /Users/sho/src/android/ArduinoFirmata/samples/SysexSampleApp/res/layout/main.xml \
+/Users/sho/src/android/ArduinoFirmata/samples/SysexSampleApp/res/values/strings.xml \
+/Users/sho/src/android/ArduinoFirmata/samples/SysexSampleApp/bin/AndroidManifest.xml \
